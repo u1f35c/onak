488ebf5104018eb86e4e33ab3178421f8df3afb8
Jonathan McDowell <noodles@earth.li>
2004-05-31 23:47:16 +0000

    cscvs to tla changeset 42 Author: noodles Date: 2002/11/22 11:45:15 Tighten up SQL referencial integrity.

--- onak.sql
+++ onak.sql
@@ -4,17 +4,21 @@ DROP TABLE onak_sigs;
 
 CREATE TABLE onak_keys (
 	keyid	char(16) NOT NULL,
-	keydata	oid NOT NULL
+	keydata	oid NOT NULL,
+	PRIMARY KEY (keyid)
 );
 CREATE INDEX onak_keys_keyid_index ON onak_keys(keyid);
 
 CREATE TABLE onak_uids (
 	keyid	char(16) NOT NULL,
 	uid	varchar(6000) NOT NULL,
-	pri	boolean
+	pri	boolean,
+	FOREIGN KEY (keyid) REFERENCES onak_keys
 );
 
 CREATE TABLE onak_sigs (
 	signer	char(16) NOT NULL,
-	signee	char(16) NOT NULL
+	signee	char(16) NOT NULL,
+	FOREIGN KEY (signee) REFERENCES onak_keys
 );
+CREATE INDEX onak_sigs_signee_index ON onak_sigs(signee);
