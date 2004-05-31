39f70717750a0f0d07d218d1de07681bfeb1b588
Jonathan McDowell <noodles@earth.li>
2004-05-31 23:47:49 +0000

    cscvs to tla changeset 87 Author: noodles Date: 2003/06/08 21:18:43 0.2.0 release. Update HISTORY, TODO, bump version in onak-conf.h/README

--- onak-conf.h
+++ onak-conf.h
@@ -5,13 +5,13 @@
  *
  * Copyright 2002 Project Purple
  *
- * $Id: onak-conf.h,v 1.9 2003/06/04 20:57:11 noodles Exp $
+ * $Id: onak-conf.h,v 1.10 2003/06/08 21:18:43 noodles Exp $
  */
 
 #ifndef __ONAK_CONF_H_
 #define __ONAK_CONF_H_
 
-#define VERSION "0.1.2"
+#define VERSION "0.2.0"
 #define CONFIGFILE "/home/noodles/projects/onak/onak.conf"
 
 /*
