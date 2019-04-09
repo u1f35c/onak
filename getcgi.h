6df51fef2960f533a741fb7290867387ed3fbba5
Jonathan McDowell <noodles@earth.li>
2019-04-09 14:54:13 +0100

    Update GPL location to URL rather than postal address

--- getcgi.h
+++ getcgi.h
@@ -13,8 +13,7 @@
  * more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 51
- * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef __GETCGI_H_
