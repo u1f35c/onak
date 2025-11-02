d8014158df323cb419b7979dc4f744ae44de695c
Jonathan McDowell <noodles@earth.li>
2025-11-02 17:10:31 +0000

    Fix sixdegrees parsing of keyids with high bit set
    Key IDs are 64-bit unsigned values, so use strtoull so we correctly
    parse keys that have their high bit set.

--- sixdegrees.c
+++ sixdegrees.c
@@ -151,7 +151,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (optind < argc) {
-		keyid = strtoll(argv[optind], NULL, 16);
+		keyid = strtoull(argv[optind], NULL, 16);
 	}
 
 	readconfig(configfile);
